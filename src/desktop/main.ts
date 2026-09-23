e228c84759c7867e34f794c26b66acd62308b3ef
Saber-Alter-Lily <3178088154@qq.com>
2026-09-22 23:35:08 -0700

    fix: enter graceful shutdown directly after HTTP finish

--- src/desktop/main.ts
+++ src/desktop/main.ts
@@ -988,7 +988,7 @@ async function startEngine(preferredPort: number) {
         browserSessionClosed,
         shutdown: () => {
             log.write('Shutdown: Desktop controller request received')
-            setImmediate(() => void stop())
+            void stop()
         }
     }
     try {

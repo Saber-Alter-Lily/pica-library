3167eed296cfc5bc4025ec33c86c11f69e738578
Saber-Alter-Lily <3178088154@qq.com>
2026-09-25 01:06:43 -0700

    fix: import Context for async detail evidence P2 G11

--- mobile/android-alpha2/app/src/main/java/com/picalibrary/android/MainActivity.java
+++ mobile/android-alpha2/app/src/main/java/com/picalibrary/android/MainActivity.java
@@ -1,6 +1,7 @@
 package com.picalibrary.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
